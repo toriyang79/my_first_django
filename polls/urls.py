75827155dfa4c645314982005f7667018045c04d
toriyang <a01037575147@gmail.com>
2025-08-25 14:03:03 +0900

    템플릿 상속

--- polls/urls.py
+++ polls/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('good/<str:name>/', views.good),  # good 뷰를 호출
     # path('debug/', views),  # debug 뷰를 호출
     path("debug/", views.debug_request),  # /polls/debug/ 로 접속
-
 ]
 #url이 어떻게 뷰로 연결되는지 원리를 이해.
